81a2ae5618e11ce81f9d51f41a0010bb829fbdb2
cambaccarini <cambaccarini01@gmail.com>
2025-02-07 10:26:09 -0300

    correccion minima

--- src/components/TablaRequerimientos.tsx
+++ src/components/TablaRequerimientos.tsx
@@ -24,7 +24,7 @@ export function TablaRequerimientos() {
       codigo: "REH-2024-000000002",
       prioridad: "MEDIA",
       tipo: "hardware",
-      categoria: "hardware",
+      categoria: "Solicitud reparación de hardware",
       fechaAlta: "14/09/2024",
       estado: "Abierto",
       asunto: "unAsunto",
