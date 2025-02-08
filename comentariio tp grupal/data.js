91bdb14d51b79536e53879a1efbf0bab8c98515e
Karga <nicolasdavidojeda@gmail.com>
2025-02-08 16:20:54 -0300

    probando comentarios

---
+++ comentariio tp grupal/data.js
@@ -0,0 +1,18 @@
+export const comentarios = [
+    {
+        id: 1,
+        emisor: "Juan Pérez",
+        titulo: "Confirmación de Turno",
+        fechaHora: "06/02/2025 14:30",
+        detalle: "Estimado paciente, su turno ha sido reservado con éxito para el 10/02/2025 a las 10:00 AM. Por favor, no olvide llevar su DNI.",
+        archivosAdjuntos: ["Comprobante_Turno.pdf", "Instrucciones_PreTurno.pdf"]
+    },
+    {
+        id: 2,
+        emisor: "María López",
+        titulo: "Resultado de Estudios",
+        fechaHora: "05/02/2025 10:15",
+        detalle: "Sus resultados de laboratorio ya están disponibles. Puede consultarlos en línea o retirarlos personalmente en la sucursal más cercana.",
+        archivosAdjuntos: ["Resultado_Laboratorio.pdf"]
+    }
+];
