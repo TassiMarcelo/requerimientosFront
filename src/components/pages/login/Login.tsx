b0cb3b919b7f9b4725ff967156ab2e5ce186a863
Ian Vidmar <68394559+DRVR1@users.noreply.github.com>
2025-02-12 22:18:48 -0300

    Register y login
    - Sweet alert para register y login
    - Conectar register al back

--- src/components/pages/login/Login.tsx
+++ src/components/pages/login/Login.tsx
@@ -1,14 +1,28 @@
 import './Login.css'
 import React, { useState, useRef } from "react";
+import Swal from 'sweetalert2';
 
 export default function Login() {
 
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  //login
+  const [loginUsername, setloginUsername] = useState("");
+  const [loginPassword, setloginPassword] = useState("");
   const [error, setError] = useState("");
+
+  // register
+  const [nombre, setNombre] = useState("");
+  const [apellido, setApellido] = useState("");
+  const [email, setEmail] = useState("");
+  const [registerPassword, setRegisterPassword] = useState("");
+  const [registerUsername, setRegisterUsername] = useState("");
+  const [cuil, setCuil] = useState("");
+  const [descripcion, setDescripcion] = useState("");
+  const [empresa, setEmpresa] = useState("");
   
   const handleLogin = async () => {
       try {
+        var username = loginUsername;
+        var password = loginPassword;
         const response = await fetch("http://localhost:8080/auth/login", {
           method: "POST",
           headers: {
@@ -23,11 +37,37 @@ export default function Login() {
         
         const data = await response.json();
         console.log("Login success:", data);
+        Swal.fire("Sesion iniciada");
       } catch (error) {
         setError("Error logging in");
         console.error("Login error:", error);
       }
   };
+
+  const handleRegister = async () => {
+    try {
+      var username = registerUsername;
+      var password = registerPassword;
+      const response = await fetch("http://localhost:8080/usuarios/registrar", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ nombre, apellido, email, password,username,cuil,descripcion,empresa }),
+      });
+      
+      if (!response.ok) {
+        throw new Error("Reg failed");
+      }
+      
+      const data = await response.json();
+      console.log("Register success:", data);
+      Swal.fire("Exito","Usuario registrado con exito");
+    } catch (error) {
+      setError("Error Registering");
+      console.error("Register error:", error);
+    }
+};
 
   const [isLogin, setIsLogin] = useState(true);
   const passwordRef = useRef<HTMLInputElement>(null);
@@ -54,8 +94,8 @@ export default function Login() {
         {isLogin ? (
           <form>
             <h2>Iniciar sesión</h2>
-            <input type="username" onChange={(e) => setUsername(e.target.value)} name="username" placeholder="Nombre de usuario" required />
-            <input ref={passwordRef} onChange={(e) => setPassword(e.target.value)} type="password" name="password" placeholder="Contraseña" required />
+            <input type="username" onChange={(e) => setloginUsername(e.target.value)} name="username" placeholder="Nombre de usuario" required />
+            <input ref={passwordRef} onChange={(e) => setloginPassword(e.target.value)} type="password" name="password" placeholder="Contraseña" required />
             <div className="showPasswordDiv">
               <input type="checkbox" onChange={togglePasswordVisibility} />
               <label>Mostrar contraseña</label>
@@ -72,32 +112,38 @@ export default function Login() {
           <form>
             <h2>Registrarse</h2>
             <label>Nombre*</label>
-            <input type="text" placeholder="Nombre" required />
+            <input type="text" onChange={(e) => setNombre(e.target.value)} placeholder="Nombre" required />
 
             <label>Apellido*</label>
-            <input type="text" placeholder="Apellido" required />
+            <input type="text" onChange={(e) => setApellido(e.target.value)} placeholder="Apellido" required />
 
-            <label>DNI*</label>
-            <input type="text" placeholder="DNI" required />
+            <label>Cuil*</label>
+            <input type="text" onChange={(e) => setCuil(e.target.value)} placeholder="CUIL" required />
 
-            <label>Teléfono*</label>
-            <input type="text" placeholder="Teléfono" required />
+            <label>Nombre de usuario*</label>
+            <input type="text" onChange={(e) => setRegisterUsername(e.target.value)} placeholder="Nombre de usuario" required />
+
+            <label>Descripcion</label>
+            <input type="text" onChange={(e) => setDescripcion(e.target.value)} placeholder="Descripcion" required />
+
+            <label>Empresa*</label>
+            <input type="text" onChange={(e) => setEmpresa(e.target.value)} placeholder="Empresa" required />
 
             <label>Correo Electrónico*</label>
-            <input type="email" placeholder="Correo Electrónico" required />
+            <input type="email" onChange={(e) => setEmail(e.target.value)} placeholder="Correo Electrónico" required />
 
             <label>Contraseña*</label>
-            <input ref={passwordRef} type="password" placeholder="Contraseña" required />
+            <input ref={passwordRef} onChange={(e) => setRegisterPassword(e.target.value)}  type="password" placeholder="Contraseña" required />
 
             <label>Repetir contraseña*</label>
-            <input ref={confirmPasswordRef} type="password" placeholder="Repetir contraseña" required />
+            <input ref={confirmPasswordRef}  type="password" placeholder="Repetir contraseña" required />
 
             <div className="showPasswordDiv">
               <input type="checkbox" onChange={togglePasswordVisibility} />
               <label>Mostrar contraseña</label>
             </div>
 
-            <button type="button">Registrarse</button>
+            <button type="button" onClick={handleRegister}>Registrarse</button>
             <h6>
               ¿Ya tenés cuenta?{" "}
               <button type="button" className="toggler" onClick={toggleForm}>
