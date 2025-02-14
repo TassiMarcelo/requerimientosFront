d8381d8d4644af1bf2588cc147973cd973361dd3
Ian Vidmar <68394559+DRVR1@users.noreply.github.com>
2025-02-14 16:14:54 -0300

    Merge ambos backs

---
+++ src/types/user.ts
@@ -0,0 +1,15 @@
+export interface User {
+    id: string
+    cuil: string
+    email: string
+    nombre: string
+    apellido: string
+    empresa: string
+    descripcion: string
+    preferencia: boolean
+    username: string
+    password: string
+    activado: any
+    role: any
+  }
+  
