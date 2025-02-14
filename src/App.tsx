35f49a38b1551f5b7899064d6e7197568bf3ef9e
Ian Vidmar <68394559+DRVR1@users.noreply.github.com>
2025-02-14 17:03:32 -0300

    Juntar los dos proyectos de front
    - Falta arreglar que requerimientosFront\src\components\pages\login.css esta interfiriendo con todos los css del front

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import { TablaRequerimientos } from './components/TablaRequerimientos'
 import Login from './components/pages/login/Login'
+import { UserTable } from './components/user-table'
+import { ThemeProvider } from "./components/theme-provider"
+import UserMenu from './components/ui/UserMenu';
+import Usuarios from './components/pages/gestionarUsuarios/Usuarios'
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
@@ -10,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<TablaRequerimientos />} />
         <Route path="/login" element={<Login />} />
+        <Route path='/gestionarUsuarios' element={<Usuarios/>} />
       </Routes>
     </Router>
   );
---
+++ src/components/pages/gestionarUsuarios/Usuarios.tsx
@@ -0,0 +1,13 @@
+
+import UserMenu from '../../ui/UserMenu';
+import { UserTable } from '../../user-table';
+
+export default function Usuarios() {
+  return (
+    <div>
+        <h1 className="text-3xl font-bold mb-6">Gestión De Usuarios</h1>
+        <UserMenu userName={"ola"} />
+        <UserTable />
+    </div>
+  );
+}
