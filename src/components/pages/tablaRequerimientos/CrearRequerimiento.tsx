df5d2bdaedf21912d14de6138cbc01dfa3dbf048
Ian Vidmar <68394559+DRVR1@users.noreply.github.com>
2025-02-23 09:37:58 -0300

    Archivos al crear requerimiento

--- src/components/pages/tablaRequerimientos/CrearRequerimiento.tsx
+++ src/components/pages/tablaRequerimientos/CrearRequerimiento.tsx
@@ -36,6 +36,15 @@ export function CrearRequerimiento({ onCrear, isOpen, onClose, datos }: CrearReq
   const [tipos, setTipos] = useState([]);
   const [categorias, setCategorias] = useState([]);
 
+  const [archivos, setArchivos] = useState<File[]>([])
+  const fileInputRef = useRef<HTMLInputElement>(null)
+
+  const [showCancelConfirmation, setShowCancelConfirmation] = useState(false);
+
+  const [selectedOption, setSelectedOption] = useState<{ value: string; label: string }[]>([]);
+
+
+
   // Cargar tipos y categorías
   useEffect(() => {
     const cargarDatosIniciales = async () => {
@@ -101,10 +110,14 @@ export function CrearRequerimiento({ onCrear, isOpen, onClose, datos }: CrearReq
     const jsonBlob = new Blob([JSON.stringify(jsonData)], { type: "application/json" });
     const jsonFile = new File([jsonBlob], "datos.json", { type: "application/json" });
 
+    const formData = new FormData();
+    for(const arch of archivos){
+      formData.append("archivos", arch);
+    }
     const file1 = new File([], "archivo_vacio.txt", { type: "text/plain" });
   
     // Crear FormData para enviarlo
-    const formData = new FormData();
+    
     formData.append("requerimientoDTO", jsonFile); // El backend debe esperar una clave "file"
     formData.append("archivos", file1); // El backend debe esperar una clave "file"
   
@@ -128,10 +141,6 @@ export function CrearRequerimiento({ onCrear, isOpen, onClose, datos }: CrearReq
   };
 
 
-  const [archivos, setArchivos] = useState<File[]>([])
-  const fileInputRef = useRef<HTMLInputElement>(null)
-
-  const [showCancelConfirmation, setShowCancelConfirmation] = useState(false);
 
   const obtenerOpcionesCategoria = (tipoSeleccionado: string) => {
     if (!tipoSeleccionado) {
@@ -231,7 +240,6 @@ const handleCategoriaChange = (selected: any) => {
   })) : [];
 
 
-  const [selectedOption, setSelectedOption] = useState<{ value: string; label: string } | null>(null); // requerimientos relacionados
 
   const handleCancel = () => {
     Swal.fire({
